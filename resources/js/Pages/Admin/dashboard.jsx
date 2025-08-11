3a9696a1fd4a46970bd80c055d622a7c764657df
Ark <istorya707@gmail.com>
2025-08-11 17:48:29 +0800

    qcv``

--- resources/js/Pages/Admin/dashboard.jsx
+++ resources/js/Pages/Admin/dashboard.jsx
@@ -1,25 +1,9 @@
-// resources/js/Pages/Admin/dashboard.jsx
-import React from "react";
-import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
-import Links from "@/Components/admin/Links";
-import { Head } from "@inertiajs/react";
+import React from 'react'
 
-export default function Dashboard() {
+function Dashboard() {
   return (
-    <AuthenticatedLayout
-      header={<h2 className="font-semibold text-xl text-gray-800 dark:text-gray-200">Admin Dashboard</h2>}
-    >
-      <Head title="Admin Dashboard" />
-
-      <div className="flex">
-        <aside className="w-64 bg-gray-100 dark:bg-gray-900 min-h-screen">
-          <Links />
-        </aside>
-
-        <main className="flex-1 p-6">
-          Dashboard Content Here
-        </main>
-      </div>
-    </AuthenticatedLayout>
-  );
+    <div>Dashboard</div>
+  )
 }
+
+export default Dashboard
