e56fa6fe818b915a33ef08a34f0f4da1d29ce0b5
maze272003 <jg.jonatas.au@phinmaed.com>
2025-08-11 16:59:07 +0800

    admins navigations for their tools and etc

--- resources/js/Pages/Admin/History.jsx
+++ resources/js/Pages/Admin/History.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import Links from "@/Components/admin/Links";
+import { Head } from "@inertiajs/react";
 
-function History() {
+export default function History() {
   return (
-    <div>History</div>
-  )
-}
+    <AuthenticatedLayout
+      header={<h2 className="font-semibold text-xl text-gray-800 dark:text-gray-200">History</h2>}
+    >
+      <Head title="History" />
 
-export default History
+      <div className="flex">
+        <aside className="w-64 bg-gray-100 dark:bg-gray-900 min-h-screen">
+          <Links />
+        </aside>
+
+        <main className="flex-1 p-6">
+          History Content Here
+        </main>
+      </div>
+    </AuthenticatedLayout>
+  );
+}
