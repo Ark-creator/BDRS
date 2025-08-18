7a3397562fd4ed6077a5edf993662ea84372c3f8
John Michael Jonatas <johnmichaeljonatas@Johns-MacBook-Pro.local>
2025-08-18 19:44:48 +0800

    dev tool disable for productions

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -7,6 +7,29 @@ import { createRoot } from 'react-dom/client';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
+// uncomment this if our project is already deploy it will help to prevent to our sessions activity and etc....
+
+// // 🚫 Disable DevTools, Right-Click, View Source
+// document.addEventListener("contextmenu", (e) => e.preventDefault());
+
+// document.addEventListener("keydown", (e) => {
+//     if (
+//         e.key === "F12" || 
+//         (e.ctrlKey && e.shiftKey && (e.key === "I" || e.key === "J")) ||
+//         (e.ctrlKey && e.key === "U") // Ctrl+U
+//     ) {
+//         e.preventDefault();
+//         alert("Developer tools are disabled on this site.");
+//     }
+// });
+
+// setInterval(() => {
+//     if (window.outerWidth - window.innerWidth > 200 || 
+//         window.outerHeight - window.innerHeight > 200) {
+//         document.body.innerHTML = "<h1>🚫 DevTools not allowed 🚫</h1>";
+//     }
+// }, 1000);
+
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
     resolve: (name) =>
